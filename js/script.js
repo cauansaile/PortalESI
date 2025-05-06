ee9fceaf0eba574a8bece8b69350a41906380caf
cauansaile1 <cauansaile1@gmail.com>
2025-05-06 14:01:30 -0300

    finalizei header e nav

---
+++ js/script.js
@@ -0,0 +1,6 @@
+
+const menuBtn = document.querySelector('.menu')
+const menusand = document.querySelector('.menu-sand') 
+    menuBtn.addEventListener('click', () => {
+    menusand.classList.toggle('active');
+    });
