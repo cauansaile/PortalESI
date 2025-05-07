c535f0bfacf2b16507cf51c8ad0e0b4aa89c41a0
cauansaile1 <cauansaile1@gmail.com>
2025-05-07 11:35:37 -0300

    finalizei páginas dos educadores

--- js/script.js
+++ js/script.js
@@ -4,3 +4,21 @@ const menusand = document.querySelector('.menu-sand')
     menuBtn.addEventListener('click', () => {
     menusand.classList.toggle('active');
     });
+
+
+function abrirLightbox(el){
+    const imgSrc = el.querySelector('img').getAttribute('src')
+    const descricao = el.getAttribute('descricao-legenda')
+
+    document.getElementById('lightbox-img').src = imgSrc
+    document.getElementById('lightbox-texto').innerText = descricao
+    document.getElementById('lightbox').classList.add('active')
+    descricao = document.body.style.color
+
+}
+
+function fecharLightbox(event){
+    const lightbox = document.getElementById('lightbox')
+    if (event.target.id === 'lightbox' || event.target.classList.contains('close-btn')) {
+        lightbox.classList.remove('active')
+}}
